2f44935e66dddfbae132f58ea219cefc263bc71a
apbs <aparnabs.prasad@gmail.com>
2019-03-06 23:56:37 +0100

    test check

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "airbnb",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true,
+            "impliedStrict": true
+        },
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        // Indent with 4 spaces
+        "indent": ["error", 4],
+
+        // Indent JSX with 4 spaces
+        "react/jsx-indent": ["error", 4],
+
+        // Indent props with 4 spaces
+        "react/jsx-indent-props": ["error", 4],
+    }
+};
--- test/App.test.js
+++ test/App.test.js
@@ -1,5 +1,6 @@
 import App from '../src/Components/App/App'
 import React from 'react'
+import ReactDOM from "react-dom";
 import { shallow } from 'enzyme'
 
 describe('App', () => {
@@ -8,3 +9,9 @@ describe('App', () => {
     expect(wrapper).toMatchSnapshot
   })
 })
+
+/*it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<Provider><App/></Provider>, div);
+  ReactDOM.unmountComponentAtNode(div);
+});*/
